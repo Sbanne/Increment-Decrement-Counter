dca2f405383720f2b22cc520ef16694007ba2a9c
Sujit Banne <sujitbanne13@gmail.com>
2022-12-27 11:37:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css';
+
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button className='btn1' onClick={() => setCount(count + 1)}>Increment</button>
+      <button className='btn2' onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  )
+}
